00b388b92f39d65d84aa1b424a9ca6cc2565a20c
Nelly-Mawira <nelliusmawira@gmail.com>
2024-07-08 11:51:02 +0300

    Add files via upload

---
+++ shoes/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def index(request):
+    return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def services(request):
+    return render(request, 'services.html')
+
+def contact(request):
+    return render(request, 'contact.html')
+
